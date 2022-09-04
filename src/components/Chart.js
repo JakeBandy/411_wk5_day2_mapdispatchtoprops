161a5c8b1e6ae3955f68f0a5582725f9c4c4f6ce
JakeBandy <jakebandy13@hotmail.com>
2022-09-04 14:42:07 -0500

    finish

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,7 +3,9 @@ import PieChart from 'react-minimal-pie-chart';
 
 const Chart = (props) => {
     // create variable "over" with all the cars whose horsepower is >= 200
-    // create variable "under" with all the cars whose horsepower is < 200
+    const over = props.cars.filter((car) => car.horsepower >= 200);
+  // create variable "under" with all the cars whose horsepower is < 200
+  const under = props.cars.filter((car) => car.horsepower < 200);
 
     return (
         <div>
@@ -25,24 +27,17 @@ const Chart = (props) => {
 
 function Legend() {
     return (
-        <h6>Horsepower: &nbsp;
-            <span style={{background: '#C13C37'}}>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-            </span>
-            &nbsp;
-            <span>Over 200</span>
-            &nbsp;
-            <span style={{background: '#E38627'}}>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-            </span>
-            &nbsp;
-            <span>Under 200</span>
+        <h6>
+          Horsepower: &nbsp;
+          <span style={{ background: "#C13C37" }}>&nbsp; &nbsp; &nbsp;</span>
+          &nbsp;
+          <span>Over 200</span>
+          &nbsp;
+          <span style={{ background: "#E38627" }}>&nbsp; &nbsp; &nbsp;</span>
+          &nbsp;
+          <span>Under 200</span>
         </h6>
-    )
+      );
 }
 
 export default Chart
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux'
 
 const user = (state = null) => state
-
-// add switch statements in here
 const cars = (state = [], action) => {
-    return state
+  // add switch statements in here
+switch (action.type) {
+  case "ADD_CAR":
+    return [...state, action.value];
+  case "REMOVE_CAR":
+    const copy = [...state];
+    copy.splice(action.value, 1)
+    return copy;
+  default:
+    return state;
+    }
 }
 
 export default combineReducers({ user, cars })
